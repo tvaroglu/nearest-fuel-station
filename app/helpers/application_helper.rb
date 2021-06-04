3706f65d1699a43dc4279a15e2d8d7ed5628162c
Tim Tyrrell <timomitchel23@gmail.com>
2021-06-04 10:02:55 -0600

    Update locations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def location_list
     [
-      ['Turing', '1331 17th St LL100, Denver, CO 80202']
+      ['Griffin Coffee', '5224 W 25th Ave, Denver, CO 80214'],
+      ['The Noshery', '4994 Lowell Blvd, Denver, CO 80221']
     ]
   end
 end
